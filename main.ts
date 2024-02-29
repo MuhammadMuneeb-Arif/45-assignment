25c159b159a346e7c165121200dd164633ce4848
M <161030327+MuhammadMuneeb-Arif@users.noreply.github.com>
2024-02-29 04:54:39 -0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
- //TASK 1: 
+
+ 
+//TASK 1: 
 // // completed
 // //TASK 2:
  let Name="Haider"
@@ -81,9 +83,8 @@ console.log(names[3]);
 
  //task 14:
 
- let guestArr:string []=["muheeb","ubaid","haider"];
-
- guestArr.map((items) =>(console.log( `Dear ${items} , you are invited to the dinner in our restaurent.`))) 
+  let Guest:string []=["muheeb","ubaid","haider"];
+  Guest.map((items) =>(console.log( `Dear ${items} , you are invited to the dinner in our restaurent.`))) 
  //TASK 14 COMPLETED:
 
  //task 15:
@@ -103,32 +104,47 @@ console.log(names[3]);
  //task 15 COMPLETED:-
 
  //task 16:
-
+ 
  let Guest=["Muheeb","Ubaid","Haider","Saifullah"];
 
  let absent="Saifullah"
-
  let newGuest = "Jamal"
-
  Guest [Guest.indexOf(absent)] = newGuest;
-
  console.log(Guest)
 
- Guest.map((items) => 
- console.log(`Dear ${items}, I found a bigger dinning table so I am inviting more people ` );
- task 16 part 2 beginning : 
- let Guestbeg :string="Mubeen"
- Guest.unshift(Guestbeg);
- console.log(Guest);
- //  task 16 part 3 Middile :
- let middleGuest :string  =" Hamza" 
- let middleIndex = Guest.length/3
- Guest.splice(middleIndex , 0 , middleGuest);
- console.log(Guest);
+  Guest.map((items) => 
+  console.log(`Dear ${items}, I found a bigger dinning table so I am inviting more people ` );
+ // task 16 part 2 beginning : 
+  let Guestbeg :string="Mubeen"
+  Guest.unshift(Guestbeg);
+  console.log(Guest);
+  //  task 16 part 3 Middile :
+  let middleGuest :string  =" Hamza" 
+  let middleIndex = Guest.length/3
+  Guest.splice(middleIndex , 0 , middleGuest);
+  console.log(Guest);
+                                     
 
-//  task 16 part 4  append :
+ //  task 16 part 4  append :
+ Guest.push("zeeshan");
+ console.log(guestArr);
+//task 16 part 5 :
+Guest.map((items)=> console.log(`Dear ${items} ,you are invited on my birthday party along with your family`))
 
- 
+//task 17:
+   while (Guest.length > 2 ) {
+       const removedGuest = Guest.pop();
+        console.log(`Sorry , ${removedGuest} you are no longer invited to the dinner`);
+   }
+ //messaging the existing persons:
+  Guest.forEach((Guest)=> {
+  console.log(`Dear ${Guest}, you are still invited to the dinner.`) )
+
+  }
+
+
+
+
 
 
 
