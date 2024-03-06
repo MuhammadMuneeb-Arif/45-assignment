c1c15a6cbbf0ecc9fca1a7046663a65e33f6caf4
M <161030327+MuhammadMuneeb-Arif@users.noreply.github.com>
2024-03-06 19:32:55 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 "use strict";
-// // //TASK 1: 
-// // // completed
-// // //TASK 2:
-// //  let Name="Haider"
-// //  console.log( "hello      " , Name +",",`"would you like to learn some python today? "`);
-// //  //task 3:
-// //  let personName:string=" muneeb";
-// //  console.log("uppercase:", personName.toUpperCase() );
-// //  console.log("lowercase:",personName.toLowerCase());
-// //  console.log("titlecase:",personName.replace (/\b\w/g, c=> c.toUpperCase()));
-// //   //TASK 4:
-// //   let famous_person:string="QUAID-E-AZAM"
-// //   let Quote:string="there is no power on earth that can undo pakistan"
-// //   console.log(`${famous_person} once said," ${Quote}"`);
-// //  //task 5:
-// //  let Author ="Henry Ford";
-// //  let Quote:string ="WHETHER YOU THINK YOU CAN OR THINK YOU CAN'T YOU'RE RIGHT"
-// //  console.log(`${Author} once said," ${Quote}"`);
+//TASK 1: 
+// completed
+//TASK 2:
+ let Name="Haider"
+ console.log( "hello "haider", Name +",",`"would you like to learn some python today? "`);
+ //task 3:
+ let personName:string=" muneeb";
+ console.log("uppercase:", personName.toUpperCase() );
+ console.log("lowercase:",personName.toLowerCase());
+ console.log("titlecase:",personName.replace (/\b\w/g, c=> c.toUpperCase()));
+  //TASK 4:
+  let famous_person:string="QUAID-E-AZAM"
+  let Quote:string="there is no power on earth that can undo pakistan"
+  console.log(`${famous_person} once said," ${Quote}"`);
+ //task 5:
+ let Author ="Henry Ford";
+ let Quote:string ="WHETHER YOU THINK YOU CAN OR THINK YOU CAN'T YOU'RE RIGHT"
+ console.log(`${Author} once said," ${Quote}"`);
 function createCar(manufacture, model, optional) {
     return {
         manufacture,
